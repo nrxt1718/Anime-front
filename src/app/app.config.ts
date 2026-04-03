d87c5782e7340948749648e27dd8ba3442cb750a
MATHIEU Alexis <alexis.mathieu@citeo.com>
2026-04-03 11:31:39 +0200

    Appel à l'API Jikan et fix ngPrime

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,19 @@
 import {ApplicationConfig, provideBrowserGlobalErrorListeners} from '@angular/core';
 import {provideRouter} from '@angular/router';
 import {providePrimeNG} from 'primeng/config';
-import material from '@primeuix/themes/material';
+import Aura from '@primeuix/themes/aura';
 import {routes} from './app.routes';
 import {provideHttpClient} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
-    provideHttpClient(),
     providePrimeNG({
       theme: {
-        preset: material
+        preset: Aura
       }
-    })
+    }),
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideHttpClient()
   ]
 };
