131004bb1fe7ba5ce59ed48840a01eca8eeacada
Noah <noah.hilberger@outlook.com>
2026-04-03 11:13:46 +0200

    ajout de plus d'animer et tjr le bug d'affichage

--- src/anime-list/anime-list.ts
+++ src/anime-list/anime-list.ts
@@ -1,4 +1,4 @@
-import {Component, inject, Injectable, OnInit, signal} from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {RouterLink} from '@angular/router';
 import { ButtonModule } from 'primeng/button';
